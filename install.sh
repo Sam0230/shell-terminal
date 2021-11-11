5e2cde24a42a3dc0a578867bdd3726ed81e8279f
Sam0230 <stoned-young0x@icloud.com>
2021-11-11 18:28:47 +0800

    bug fix

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ if test "${BASH_SOURCE[0]}"; then
 else
 	pushd "$(dirname "$0")"
 fi
-for i in bash_*; do
+for i in bash*; do
 	mv ~/".$i" ~/".$i.old" 2>/dev/null
-	cp -r "$i" ~/".$i"
+	cp -rf "$i" ~/".$i"
 done
 popd
